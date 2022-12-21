4c50b030b015660ad300ee5a876ef4a9bf946b79
Andrey Popov <a.a.popov@student.khai.edu>
2022-12-21 17:01:06 +0000

    Credit Calculator v1.0

---
+++ credit_calculator/credit_calculator.py
@@ -0,0 +1,11 @@
+"""Credit calculator. by Popov Andrey"""
+from super_calc import SuperCreditCalc
+
+
+def main():
+    """Credit calculator entry point."""
+    calc = SuperCreditCalc()
+    calc.load()
+
+if __name__ == "__main__":
+    main()
